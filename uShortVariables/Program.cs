a55c509ef271f31ca534f966689e34a2f6e8f749
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-16 19:20:00 +0300

    Proje dosyası ekle.

---
+++ uShortVariables/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uShortVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ushort sayi1,sayi2,toplam;
+            //sayi1 = 30;
+            //sayi2 = 10;
+            //toplam =(ushort)(sayi1 + sayi2);
+            //Console.WriteLine(toplam);
+
+            //klavyeden girilen cevre ve alan hesaplamaları (dikdörtgen)
+            ushort kisaKenar, uzunKenar, cevre, alan;
+            Console.WriteLine("Lütfen dikdörtgenin kısa kenar ölçüsünü girin : ");
+            kisaKenar = ushort.Parse(Console.ReadLine());
+            Console.WriteLine("Lütfen dikdörtgenin uzun kenar ölçüsünü girin : ");
+            uzunKenar = ushort.Parse(Console.ReadLine());
+            cevre = (ushort)(2*(kisaKenar+uzunKenar));
+            alan = (ushort)(uzunKenar*kisaKenar);
+            Console.WriteLine("Cevre : " + cevre);
+            Console.WriteLine("Alan : " + alan);
+            
+            Console.Read();
+        }
+    }
+}
